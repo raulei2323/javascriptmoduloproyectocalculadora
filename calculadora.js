aec26fd18d0d95ab24bebf880ce5a93c94c1861d
Raul Elizalde <raulei2323@gmail.com>
2024-02-28 01:13:36 -0600

    commited to go

--- calculadora.js
+++ calculadora.js
@@ -1,11 +1,6 @@
-const deviceCal
+document.addEventListener('DOMContentLoaded', () => {
 
-operations (num1, num2) {
-  typeof num1 !=== 'Number' 'ingresa numeros'
-  typeof num2 !=== 'Number' 'ingresa numeros'
+  const numeber0 = document.getElementsByClassName('num0')
 
   
-
-
-}
-
+})
